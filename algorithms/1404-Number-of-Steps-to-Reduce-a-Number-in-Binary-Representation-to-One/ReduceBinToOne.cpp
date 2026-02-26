c0e4ff4233d7f9880a1cc206fa7ae325c43cbb7f
Eduardo Tavares <eduardo.tavares002@edu.pucrs.br>
2026-02-26 20:53:54 -0300

    (daily) #1404 - Number of Steps to Reduce a Number in Binary Representation to One

---
+++ algorithms/1404-Number-of-Steps-to-Reduce-a-Number-in-Binary-Representation-to-One/ReduceBinToOne.cpp
@@ -0,0 +1,69 @@
+/*
+Given the binary representation of an integer as a string s, return the number of steps to reduce it to 1 under the following rules:
+
+    If the current number is even, you have to divide it by 2.
+
+    If the current number is odd, you have to add 1 to it.
+
+It is guaranteed that you can always reach one for all test cases.
+
+
+Example 1:
+
+Input: s = "1101"
+Output: 6
+Explanation: "1101" corressponds to number 13 in their decimal representation.
+Step 1) 13 is odd, add 1 and obtain 14.
+Step 2) 14 is even, divide by 2 and obtain 7.
+Step 3) 7 is odd, add 1 and obtain 8.
+Step 4) 8 is even, divide by 2 and obtain 4.
+Step 5) 4 is even, divide by 2 and obtain 2.
+Step 6) 2 is even, divide by 2 and obtain 1.
+*/
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+public:
+    int numSteps(string s)
+    {
+        int count = 0;
+        int carry = 0;
+        for (int i = s.length() - 1; i > 0; --i)
+        {
+            // ascii table: 0 = 48 | 1 = 49
+            int bit = s[i] - '0';
+            int sum = bit + carry;
+            if (sum == 0)
+            {
+                ++count;
+                carry = 0;
+            }
+            else if (sum == 1)
+            {
+                count += 2;
+                carry = 1;
+            }
+            else if (sum == 2)
+            {
+                ++count;
+                carry = 1;
+            }
+        }
+        return count + carry;
+    }
+};
+
+int main()
+{
+    Solution solution;
+    string strBin = "1101";
+
+    int iNumSteps = solution.numSteps(strBin);
+    cout << iNumSteps;
+
+    return 0;
+}
